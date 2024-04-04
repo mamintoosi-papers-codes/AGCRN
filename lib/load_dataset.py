a9ced08b3faf6e2107ba5b66348c92597088c6d0
Mahmood Amintoosi <mamintoosi@users.noreply.github.com>
2024-04-04 21:16:42 +0330

    Edit load_dataset

--- lib/load_dataset.py
+++ lib/load_dataset.py
@@ -4,10 +4,10 @@ import numpy as np
 def load_st_dataset(dataset):
     #output B, N, D
     if dataset == 'PEMSD4':
-        data_path = os.path.join('../data/PeMSD4/pems04.npz')
+        data_path = os.path.join('../data/PEMSD4/pems04.npz')
         data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
     elif dataset == 'PEMSD8':
-        data_path = os.path.join('../data/PeMSD8/pems08.npz')
+        data_path = os.path.join('../data/PEMSD8/pems08.npz')
         data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
     else:
         raise ValueError
