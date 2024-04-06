d22451c41a8e86c764155654e7f6fca0cccb60ee
Mahmood Amintoosi <mamintoosi@users.noreply.github.com>
2024-04-06 13:53:35 +0330

    Update AGCN.py
    Add comments

--- model/AGCN.py
+++ model/AGCN.py
@@ -6,21 +6,24 @@ class AVWGCN(nn.Module):
     def __init__(self, dim_in, dim_out, cheb_k, embed_dim):
         super(AVWGCN, self).__init__()
         self.cheb_k = cheb_k
-        self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, dim_in, dim_out))
-        self.bias_pool = nn.Parameter(torch.FloatTensor(embed_dim, dim_out))
+        self.weights_pool = nn.Parameter(torch.FloatTensor(embed_dim, cheb_k, dim_in, dim_out)) # W_g
+        self.bias_pool = nn.Parameter(torch.FloatTensor(embed_dim, dim_out)) # b_g
     def forward(self, x, node_embeddings):
         #x shaped[B, N, C], node_embeddings shaped [N, D] -> supports shaped [N, N]
         #output shape [B, N, C]
-        node_num = node_embeddings.shape[0]
+        node_num = node_embeddings.shape[0] # node_embeddings is E_A or E_g
         supports = F.softmax(F.relu(torch.mm(node_embeddings, node_embeddings.transpose(0, 1))), dim=1)
-        support_set = [torch.eye(node_num).to(supports.device), supports]
+        support_set = [torch.eye(node_num).to(supports.device), supports] # Part 1 Z in eq 6
         #default cheb_k = 3
         for k in range(2, self.cheb_k):
             support_set.append(torch.matmul(2 * supports, support_set[-1]) - support_set[-2])
-        supports = torch.stack(support_set, dim=0)
+        supports = torch.stack(support_set, dim=0) # Estimated Adj Mat
+        # Prob eq 4
         weights = torch.einsum('nd,dkio->nkio', node_embeddings, self.weights_pool)  #N, cheb_k, dim_in, dim_out
         bias = torch.matmul(node_embeddings, self.bias_pool)                       #N, dim_out
+        # Part 1 eq 4, until 
         x_g = torch.einsum("knm,bmc->bknc", supports, x)      #B, cheb_k, N, dim_in
         x_g = x_g.permute(0, 2, 1, 3)  # B, N, cheb_k, dim_in
-        x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out
+        # Eq 4, 6
+        x_gconv = torch.einsum('bnki,nkio->bno', x_g, weights) + bias     #b, N, dim_out 
         return x_gconv
